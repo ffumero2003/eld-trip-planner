a8eb04fb8dbff06cc18efb6dca648ccb5a69bcca
ffumero2003 <felipefumerom@gmail.com>
2026-07-02 11:23:53 -0600

    Remove old hardcoded Django settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,14 +31,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aj(qc+qma)bkj#r5qk89vcftz6mf@31#6%_(@j-*#ttl6t0)!s'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
